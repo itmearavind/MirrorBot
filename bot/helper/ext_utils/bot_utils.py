091417619d8fd743ea34190209661756c8f92813
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-08-07 10:47:18 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -133,7 +133,7 @@ def get_readable_message():
                             f", <b>ETA :</b> {download.eta()} "
                     # if hasattr(download, 'is_torrent'):
                     try:
-                        msg += f"\n<b>Seeders :</b> {download.aria_download().num_seeders}" 
+                        msg += f"\n<b>Seeders :</b> {download.aria_download().num_seeders}" \
                             f" | <b>Peers :</b> {download.aria_download().connections}"
                     except:
                         pass
